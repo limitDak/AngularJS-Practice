8508fe76e330256f9b693707d5f1194fa536f0ea
limitDak <34105863+limitDak@users.noreply.github.com>
2023-01-05 11:37:57 -0600

    Added AngularJS files

---
+++ app.js
@@ -0,0 +1,7 @@
+(function () {
+  "use strict";
+  //define module with name, then dependencies module will need in array
+  angular
+    .module("myFirstApp", [])
+    .controller("MyFirstController", function () {});
+})();
